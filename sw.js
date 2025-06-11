ff561835ec58f0f08ee2b9f36273c426184eaf29
RikoFrndsyh <139275643+RikoFrndsyh@users.noreply.github.com>
2025-06-11 17:04:45 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", event => {
   self.skipWaiting();
   event.waitUntil((async () => {
-    const cache = await caches.open("siakad-riko-cache-v2");
+    const cache = await caches.open("siakad-riko-cache");
     try {
       await cache.addAll([
         "./",
@@ -23,7 +23,7 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys.filter(key => key !== "siakad-riko-cache-v2").map(key => caches.delete(key))
+        keys.filter(key => key !== "siakad-riko-cache").map(key => caches.delete(key))
       );
     })
   );
