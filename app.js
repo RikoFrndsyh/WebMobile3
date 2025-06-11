2ca583e3b69ba2e11dafff0e54934a6486c54d12
RikoFrndsyh <139275643+RikoFrndsyh@users.noreply.github.com>
2025-06-11 16:44:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
+// Memanggil fungsi untuk meregistrasi Service Worker
 registerSW();
 
+// Fungsi async untuk mendaftarkan service worker
 async function registerSW() {
+  // Mengecek apakah browser mendukung Service Worker
   if ('serviceWorker' in navigator) {
     try {
+      // Mendaftarkan file service worker "sw.js"
       const registration = await navigator.serviceWorker.register("sw.js");
+
     } catch (error) {
+      // Jika terjadi error saat registrasi, tampilkan pesan kesalahan
       showResult("Error saat registrasi Service Worker: " + error.message);
     }
   } else {
+    // Jika browser tidak mendukung service worker
     showResult("Service Worker tidak didukung di browser ini.");
   }
 };
 
+// Fungsi untuk menampilkan hasil atau pesan ke elemen <output>
 function showResult(text) {
   document.querySelector("output").innerHTML = text;
 }
