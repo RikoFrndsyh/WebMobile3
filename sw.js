5adaf1e69f204fa384630e922a043ff9caad2fd6
RikoFrndsyh <139275643+RikoFrndsyh@users.noreply.github.com>
2025-06-11 17:15:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
+// install service worker dan mengaktifkan service worker baru tanpa menunggu versi lama
 self.addEventListener("install", event => {
   self.skipWaiting();
+  // Menunggu hingga proses caching selesai dan membuka/membuat cache baru
   event.waitUntil((async () => {
     const cache = await caches.open("siakad-riko-cache");
     try {
+       // Menyimpan cache semua file dibawah ini
       await cache.addAll([
         "./",
         "./index.html",
@@ -14,15 +17,18 @@ self.addEventListener("install", event => {
         "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
       ]);
     } catch (err) {
+      // Menampilkan pesan error jika gagal menyimpan file
       console.error("Gagal menyimpan cache:", err);
     }
   })());
 });
 
 self.addEventListener("activate", event => {
+  // Menunggu sampai proses pembersihan cache lama selesai
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
+        // Menghapus semua cache lama yang namanya tidak cocok dengan versi sekarang
         keys.filter(key => key !== "siakad-riko-cache").map(key => caches.delete(key))
       );
     })
