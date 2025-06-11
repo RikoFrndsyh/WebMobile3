e07d23b23febccc8948d7512d668ff7435656f38
RikoFrndsyh <139275643+RikoFrndsyh@users.noreply.github.com>
2025-06-11 16:52:10 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,15 @@
+// install service worker
 self.addEventListener("install", event => {
+  // skipWaiting akan langsung mengaktifkan service worker baru tanpa menunggu versi lama nonaktif
   self.skipWaiting();
+
+  // Menunggu hingga proses caching selesai sebelum install sukses
   event.waitUntil((async () => {
-    const cache = await caches.open("siakad-riko-cache-v2");
+    // Membuat/buka cache baru dengan nama 'siakad-riko-cache'
+    const cache = await caches.open("siakad-riko-cache");
+
     try {
+      // Menyimpan cache semua file di bawah ini
       await cache.addAll([
         "./",
         "./index.html",
@@ -10,20 +17,26 @@ self.addEventListener("install", event => {
         "./css/style.css",
         "./img/riko.png",
         "./app.js",
+        // Meng-cache resource eksternal Bootstrap CSS dan JS
         "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css",
         "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
       ]);
     } catch (err) {
+      // Menampilkan pesan error jika gagal menyimpan file ke cache
       console.error("Gagal menyimpan cache:", err);
     }
   })());
 });
 
+// Event listener untuk tahap 'activate' service worker
 self.addEventListener("activate", event => {
+  // Menunggu  proses pembersihan cache lama selesai
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(
-        keys.filter(key => key !== "siakad-riko-cache-v2").map(key => caches.delete(key))
+        // Menghapus semua cache lama
+        keys.filter(key => key !== "siakad-riko-cache")
+            .map(key => caches.delete(key))
       );
     })
   );
