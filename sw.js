3136f658f1cdba9b59e8f894cb08ac18e1b09615
RikoFrndsyh <139275643+RikoFrndsyh@users.noreply.github.com>
2025-06-11 16:12:45 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,27 @@
-self.addEventListener("install", async event => {
-  const cache = await caches.open("siakad-riko-cache");
-
-  // Menyimpan resource penting (offline-first)
-  await cache.addAll([
-    "/",
-    ".index.html",
-    ".beranda.html",
-    ".css/style.css",
-    ".img/riko.png",
-    ".app.js",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
-  ]);
+self.addEventListener("install", event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open("pwa-assets");
+    try {
+      await cache.addAll([
+      "/",
+      "./index.html",
+      "./beranda.html",
+      "./css/style.css",
+      "./img/riko.png",
+      "./app.js",
+      "./https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css",
+      "./https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+      ]);
+    } catch (err) {
+      console.error("Gagal menyimpan cache:", err);
+    }
+  })());
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
