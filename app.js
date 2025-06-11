17c95fa73775a6274cb2223930a55065d31cae3f
RikoFrndsyh <139275643+RikoFrndsyh@users.noreply.github.com>
2025-06-11 16:21:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ async function registerSW() {
   if ('serviceWorker' in navigator) {
     try {
       const registration = await navigator.serviceWorker.register("sw.js");
-      showResult("Service Worker registered");
     } catch (error) {
       showResult("Error saat registrasi Service Worker: " + error.message);
     }
   } else {
     showResult("Service Worker tidak didukung di browser ini.");
   }
-}
+};
 
 function showResult(text) {
   document.querySelector("output").innerHTML = text;
